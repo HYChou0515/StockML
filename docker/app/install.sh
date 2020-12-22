21e0d842d8187ff436d134173a7b49826e993dc8
Chou Hung-Yi <s1243221@gmail.com>
2020-12-23 01:40:22 +0800

    config github action

--- docker/app/install.sh
+++ docker/app/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python -m pip install pip &&
-pip install -r /tmp/requirements.txt &&
+pip install -r /StockML/docker/app/requirements.txt &&
 
 cd /StockML/lib/twstock &&
 FLIT_ROOT_INSTALL=1 flit install &&
