536362ec21505a20b3f36b48b93bea7f256e9eb4
Chou Hung-Yi <s1243221@gmail.com>
2020-12-23 17:15:02 +0800

    --

--- docker/app/install.sh
+++ docker/app/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo h1 $WINDSCRIBE_USR
+echo h2 $WINDSCRIBE_PASS
 envsubst < /windscribe_login.template > /windscribe_login &&
 python -m pip install pip &&
 pip install -r /StockML/docker/app/requirements.txt &&
@@ -7,5 +9,8 @@ pip install -r /StockML/docker/app/requirements.txt &&
 cd /StockML/lib/twstock &&
 FLIT_ROOT_INSTALL=1 flit install &&
 cd ../.. &&
+
+/etc/init.d/windscribe-cli start &&
+expect /windscribe_login &&
 
 echo "done"
