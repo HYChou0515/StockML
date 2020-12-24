c8f685735362c4ed4da6cd5408e7297a5ce93429
Chou Hung-Yi <s1243221@gmail.com>
2020-12-24 17:23:40 +0800

    use proxy instead of vpn

--- docker/base/install.sh
+++ docker/base/install.sh
@@ -2,20 +2,6 @@
 
 apt-get update &&
 
-apt-get install -y gnupg apt-utils debconf-utils dialog gettext-base &&
-
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections &&
-echo "resolvconf resolvconf/linkify-resolvconf boolean false" | debconf-set-selections &&
-
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7 &&
-echo 'deb https://repo.windscribe.com/ubuntu bionic main' | tee /etc/apt/sources.list.d/windscribe-repo.list &&
-
-apt-get update &&
-
-apt-get install -y windscribe-cli expect resolvconf &&
-
-cp /tmp/windscribe_login /windscribe_login.template &&
-
 rm -rf /var/lib/apt/lists/* &&
 
 echo "done"
--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+proxy = set()
+with open('https_proxy', 'r') as f:
+	for line in f.readlines():
+		proxy.add(line.strip())
+proxy = [{'https': p} for p in proxy]
+
 import stock
-df = stock.StockInfoGetter.get_stock(2020, 11)
+fetcher = TwseFetcher(proxy)
+df = stock.StockInfoGetter.get_stock(fetcher, 2020, 11)
 import pickle
 pickle.dump(df, open('df.pkl', 'wb'))
 
